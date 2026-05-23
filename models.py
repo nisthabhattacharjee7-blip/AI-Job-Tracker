f878b1cdb3c87a7943bf0760390ba075f5d28400
Nistha <nisthabhattacharjee7@gmail.com>
2026-05-23 18:52:14 +0530

    fixed the errors

--- models.py
+++ models.py
@@ -1,11 +1,10 @@
-from sqlalchemy import column , integer , string
+from sqlalchemy import Column , Integer , String
 from database import Base
 
 class Application(Base):
     __tablename__ = "applications"
 
-    id = column(integer, primary_key=True, index=True)
-    company_name = column(string, index=True)
-    role = column(string, index=True)
-    status = column(string, index=True)
-    
+    id = Column(Integer, primary_key=True, index=True)
+    company_name = Column(String, index=True)
+    role = Column(String, index=True)
+    status = Column(String, index=True)
