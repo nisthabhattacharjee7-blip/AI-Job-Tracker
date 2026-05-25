38418714673730ca0bf0ac37231d9349492100c6
Nistha <nisthabhattacharjee7@gmail.com>
2026-05-25 15:30:42 +0530

    added keywords

---
+++ backend/ats/keywords.py
@@ -0,0 +1,16 @@
+TECH_KEYWORDS = [
+    "python",
+    "sql",
+    "fastapi",
+    "machine learning",
+    "api",
+    "git",
+    "github",
+    "data analysis",
+    "backend",
+    "javascript",
+    "react",
+    "docker",
+    "html",
+    "css"
+]
